6cdd264886c068287ecc27d08a54030b2bbf22e0
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2024-09-16 20:51:50 +0300

    Update GenericMediaData.ts

--- src/backends/generic-media/GenericMediaData.ts
+++ src/backends/generic-media/GenericMediaData.ts
@@ -31,7 +31,7 @@ export class GenericMediaData implements GenericMediaInterface {
   };
 
   /**
-   * Fetch NFT content or retun URI if content shouild not be fetched
+   * Fetch NFT content or return URI if content should not be fetched
    * @param url NFT Content URL
    * @param contentType string mime type to fetch
    * @returns Promise<MediaContentType> Media content information or URL
